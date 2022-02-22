6d612a1b12c6cef3eded1311406d25df5f13226e
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-02-22 14:12:38 -0300

    impressao de dados do banco no console

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,33 @@
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args){
-        Connection conn = DB.getConnection();
-        DB.closeConnection();
+
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+
+            rs = st.executeQuery("select * from produto");
+
+            while (rs.next()){
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("nome"));
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        finally {
+            DB.closeResultSet(rs);
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
