3fdbb793c883aa0ae932cb92d99dffdd04ac8b7d
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-02-28 10:50:56 -0300

    teste do push

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,77 @@
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpServer;
 import db.DB;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
 
+
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/test", new MyHandler());
+        server.setExecutor(null);
+        server.start();
+
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+
+
+
+            Connection conn = null;
+            Statement st = null;
+            ResultSet rs = null;
+            try {
+                conn = DB.getConnection();
+                st = conn.createStatement();
+
+                rs = st.executeQuery("select * from produto");
+
+                while (rs.next()){
+                    System.out.println(rs.getInt("Id") + ", " + rs.getString("nome"));
+
+                    //String response = "This is the response";
+                    t.sendResponseHeaders(200, Long.parseLong(rs.getString("nome")));
+                    //t.sendResponseHeaders(200, response.length());
+                    OutputStream os = t.getResponseBody();
+                    os.write((int) Long.parseLong(rs.getString("nome")));
+                    //os.write(response.getBytes());
+                    os.close();
+                }
+            }
+            catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+
+            /*String response = "This is the response";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();*/
+        }
+    }
+
+
+    /*public static void executeQuery()  {
         Connection conn = null;
         Statement st = null;
         ResultSet rs = null;
@@ -18,7 +82,7 @@ public class Main {
             rs = st.executeQuery("select * from produto");
 
             while (rs.next()){
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("nome"));
+                 System.out.println(rs.getInt("Id") + ", " + rs.getString("nome"));
             }
         }
         catch (SQLException e) {
@@ -29,5 +93,7 @@ public class Main {
             DB.closeStatement(st);
             DB.closeConnection();
         }
-    }
+*/
+    //}
+
 }
